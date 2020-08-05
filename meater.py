60181da0371189c7042805e6dbd54682bcd97a4f
David Mc Ken <david.mcken@gmail.com>
2020-08-05 19:18:29 -0400

    re-format and added main for individual testing.

--- meater.py
+++ meater.py
@@ -1,76 +1,102 @@
 from bluepy import btle
+import datetime
 import time
+
 __all__ = ['MeaterProbe']
- 
-class MeaterProbe:
-   def __init__(self, addr):
-      self._addr = addr
-      self.connect()
-      self.update()
+
+class MeaterProbe(object):
+    def __init__(self, addr):
+        self._addr = addr
+        self.connect()
+        self.update()
        
-   @staticmethod
-   def bytesToInt(byte0, byte1):
-      return byte1*256+byte0
+    @staticmethod
+    def bytesToInt(byte0, byte1):
+        return byte1*256+byte0
 
-   @staticmethod
-   def convertAmbient(array): 
-      tip = MeaterProbe.bytesToInt(array[0], array[1])
-      ra  = MeaterProbe.bytesToInt(array[2], array[3])
-      oa  = MeaterProbe.bytesToInt(array[4], array[5])
-      return int(tip+(max(0,((((ra-min(48,oa))*16)*589))/1487)))
+    @staticmethod
+    def convertAmbient(array): 
+        tip = MeaterProbe.bytesToInt(array[0], array[1])
+        ra  = MeaterProbe.bytesToInt(array[2], array[3])
+        oa  = MeaterProbe.bytesToInt(array[4], array[5])
+        return int(tip+(max(0,((((ra-min(48,oa))*16)*589))/1487)))
       
-   @staticmethod
-   def toCelsius(value):
-      return (float(value)+8.0)/16.0
+    @staticmethod
+    def toCelsius(value):
+        return (float(value)+8.0)/16.0
 
-   @staticmethod
-   def toFahrenheit(value):
-      return ((MeaterProbe.toCelsius(value)*9)/5)+32.0
+    @staticmethod
+    def toFahrenheit(value):
+        return ((MeaterProbe.toCelsius(value)*9)/5)+32.0
 
-   def getTip(self):
-      return self._tip
+    def getTip(self):
+        return self._tip
 
-   def getTipF(self):
-      return MeaterProbe.toFahrenheit(self._tip)
+    def getTipF(self):
+        return MeaterProbe.toFahrenheit(self._tip)
 
-   def getTipC(self):
-      return MeaterProbe.toCelsius(self._tip)
+    def getTipC(self):
+        return MeaterProbe.toCelsius(self._tip)
 
-   def getAmbientF(self):
-      return MeaterProbe.toFahrenheit(self._ambient)
+    def getAmbientF(self):
+        return MeaterProbe.toFahrenheit(self._ambient)
 
-   def getAmbient(self):
-      return self._ambient
+    def getAmbient(self):
+        return self._ambient
 
-   def getAmbientC(self):
-      return MeaterProbe.toCelsius(self._ambient)
+    def getAmbientC(self):
+        return MeaterProbe.toCelsius(self._ambient)
 
-   def getBattery(self):
-      return self._battery
+    def getBattery(self):
+        return self._battery
 
-   def getAddress(self):
-      return self._addr
+    def getAddress(self):
+        return self._addr
 
-   def getID(self):
-      return self._id
+    def getID(self):
+        return self._id
 
-   def getFirmware(self):
-      return self._firmware
+    def getFirmware(self):
+        return self._firmware
 
-   def connect(self):
-      self._dev = btle.Peripheral(self._addr)
+    def connect(self):
+        self._dev = btle.Peripheral(self._addr)
 
-   def readCharacteristic(self, c):
-      return bytearray(self._dev.readCharacteristic(c))
+    def readCharacteristic(self, c):
+        return bytearray(self._dev.readCharacteristic(c))
 
-   def update(self):
-      tempBytes = self.readCharacteristic(31)
-      batteryBytes = self.readCharacteristic(35)
-      self._tip = MeaterProbe.bytesToInt(tempBytes[0], tempBytes[1])
-      self._ambient = MeaterProbe.convertAmbient(tempBytes)
-      self._battery = MeaterProbe.bytesToInt(batteryBytes[0], batteryBytes[1])*10
-      (self._firmware, self._id) = str(self.readCharacteristic(22)).split("_")
-      self._lastUpdate = time.time()
+    def update(self):
+        tempBytes = self.readCharacteristic(31)
+        batteryBytes = self.readCharacteristic(35)
+        self._tip = MeaterProbe.bytesToInt(tempBytes[0], tempBytes[1])
+        self._ambient = MeaterProbe.convertAmbient(tempBytes)
+        self._battery = MeaterProbe.bytesToInt(batteryBytes[0], batteryBytes[1])*10
+        (self._firmware, self._id) = str(self.readCharacteristic(22)).split("_")
+        self._lastUpdate = time.time()
 
-   def __str__(self):
-       return "%s %s probe: %s tip: %fF/%fC ambient: %fF/%fC battery: %d%% age: %ds" % (self.getAddress(), self.getFirmware(), self.getID(), self.getTipF(), self.getTipC(), self.getAmbientF(), self.getAmbientC(), self.getBattery(), time.time() - self._lastUpdate)
+    def __str__(self):
+        return "%s %s probe: %s tip: %fF/%fC ambient: %fF/%fC battery: %d%% age: %ds" % (
+            self.getAddress(), self.getFirmware(), self.getID(), self.getTipF(), self.getTipC(),
+            self.getAmbientF(), self.getAmbientC(), self.getBattery(), time.time() - self._lastUpdate)
+
+if __name__ == '__main__':
+    import sys
+    
+    try:
+        mac = sys.argv[1]
+    except KeyError:
+        print("Usage: {0} <mac address of Meater>".format(sys.argv[0]))
+        sys.exit(-1)
+    
+    print("Connecting to: {0}".format(mac))
+    meater = MeaterProbe(mac)
+    print("Connected!")
+    
+    while True:
+        meater.update()
+        print(meater)
+        time.sleep(1)
+    
+    
+
+
